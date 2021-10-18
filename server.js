6892ad6ae83f7aeaa2e64bb7e5e1c325609b485a
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-10-18 22:47:44 +0200

    user and password for database connection heroku

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use((req, res) => {
 const NODE_ENV = process.env.NODE_ENV;
 let dbUri = '';
 
-if(NODE_ENV === 'production') dbUri = 'mongodb+srv://WojtkoW:wojteks1@cluster0.ebl86.mongodb.net/NewWaveDB?retryWrites=true&w=majority';
+if(NODE_ENV === 'production') dbUri = 'mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.ebl86.mongodb.net/NewWaveDB?retryWrites=true&w=majority';
 
 else if(NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/NewWaveDBTest';
 else dbUri = 'mongodb://localhost:27017/NewWaveDB';
